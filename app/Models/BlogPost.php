35a50c6adb96a2945a42612f1428828eb35bb834
artday <kingvasil@gmail.com>
2019-01-15 15:02:09 +0200

    used soft delete trait and changed rout key name for model

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
 }
