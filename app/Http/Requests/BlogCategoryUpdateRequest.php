ba30b617685c1dddde90ddc32fd477ac3df23a28
artday <kingvasil@gmail.com>
2019-04-20 12:01:12 +0300

    category admin`s request added

---
+++ app/Http/Requests/BlogCategoryUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class BlogCategoryUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:5|max:200',
+//            'slug' => 'max:200|unique:blog_categories,slug,'.request()->id,
+            'slug' => ['max:200', Rule::unique('blog_categories', 'slug')->ignore($this->id)],
+            'description' => 'nullable|string|max:500|min:5',
+            'parent_id' => 'nullable|integer|exists:blog_categories,id',
+        ];
+    }
+}
