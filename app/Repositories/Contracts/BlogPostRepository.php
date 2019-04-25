0749c7f992ac4eb93581b625def1277dac372db4
artday <kingvasil@gmail.com>
2019-04-25 13:57:35 +0300

    added Blog Repository Contract

---
+++ app/Repositories/Contracts/BlogPostRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface BlogPostRepository
+{
+    /*public function allLive($paginate = 10);
+    public function allLiveLatest($paginate = 10);*/
+}
