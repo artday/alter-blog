f2e811bd7cf985161f801555838681abec28ea36
artday <kingvasil@gmail.com>
2019-04-25 14:11:27 +0300

    added few test routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', function () { return view('welcome'); })->name('home');
 
 Auth::routes();
 
@@ -10,11 +8,17 @@ Route::namespace('Blog')->prefix('blog')->name('blog.')
     ->group(function () {
         //Route::resource('category', 'CategoryController');
         Route::resource('posts', 'PostController');
+
+        Route::get('list', 'PostController@byCategories')->name('categories.posts');
+        Route::get('category/{slug}', 'PostController@categoryPosts')->name('category.posts');
     });
 
 Route::namespace('Blog\Admin')->prefix('admin/blog')->name('blog.admin.categories.')
     ->group(function () {
         Route::get('/categories', 'CategoryController@index')->name('index');
+
+        //Route::get('/categories/{category}', 'CategoryController@show')->name('show');
+
         Route::get('/categories/{category}/edit', 'CategoryController@edit')->name('edit');
         Route::put('/categories', 'CategoryController@store')->name('store');
         Route::get('/categories/create', 'CategoryController@create')->name('create');
