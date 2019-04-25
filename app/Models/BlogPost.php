e071705d0e6f3d78fdcd75f396197422b78514da
artday <kingvasil@gmail.com>
2019-04-25 13:56:47 +0300

    added user and category relations; used HasLive trait

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,16 +2,27 @@
 
 namespace App\Models;
 
+use App\Traits\Eloquent\HasLive;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasLive;
 
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(BlogCategory::class);
     }
 
 }
