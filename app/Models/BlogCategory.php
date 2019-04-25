4d0d00af27226619a1a8408033b42c311a2e7e1b
artday <kingvasil@gmail.com>
2019-04-25 13:54:06 +0300

    added model post relation; used HasLive trait

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Eloquent\HasLive;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+//use App\Repositories\Contracts\BlogCategoryRepository;
 
 class BlogCategory extends Model
 {
-//    use SoftDeletes;
+    use SoftDeletes, HasLive;
 
     protected $fillable = [
         'title', 'slug', 'description', 'parent_id'
@@ -17,5 +19,29 @@ class BlogCategory extends Model
     {
         return $this->belongsTo(BlogCategory::class);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(BlogPost::class, 'category_id');
+    }
+
+    /*public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        dump(__METHOD__);
+    }*/
+
+    /*public function resolveRouteBinding($value)
+    {
+        return app(BlogCategoryRepository::class)
+                ->resolveRouteBinding($value);
+    }*/
+
+    /*
+
+    public function boot()
+    {
+
+    }*/
 
 }
