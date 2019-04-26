5f15951f3a5863d4061a17a11250fb29d2e8317a
artday <kingvasil@gmail.com>
2019-04-26 07:03:51 +0300

    updated method`s params

--- app/Repositories/Contracts/RepositoryInterface.php
+++ app/Repositories/Contracts/RepositoryInterface.php
@@ -4,11 +4,11 @@ namespace App\Repositories\Contracts;
 
 interface RepositoryInterface
 {
-    public function all();
+    public function all($columns = ['*']);
     public function find($id);
     public function findWhere($column, $value);
     public function findWhereFirst($column, $value);
-    public function paginate($perPage = 10);
+    public function paginate($perPage = 10, $columns = ['*']);
     public function create(array $properties);
     public function update($id, array $properties);
     public function delete($id);
