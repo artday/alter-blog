a47ace18018e816387d7dab8687f64f636f2155c
artday <kingvasil@gmail.com>
2019-04-26 07:05:10 +0300

    Comments hinting added

--- app/Repositories/Contracts/BlogCategoryRepository.php
+++ app/Repositories/Contracts/BlogCategoryRepository.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Repositories\Contracts;
-
+/**
+ *  @method \Illuminate\Database\Eloquent\Collection|static[] all($columns = ['*']);
+ *  @method \Illuminate\Contracts\Pagination\LengthAwarePaginator paginate(int $perPage=10, array $columns = ['*']);
+ */
 interface BlogCategoryRepository
 {
     public function findBySlug($slug);
