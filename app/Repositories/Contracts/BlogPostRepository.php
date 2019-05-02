e7d375cf00ee5ff5ba4bdea4cd1e2d2250786ff0
artday <kingvasil@gmail.com>
2019-05-03 00:53:21 +0300

    added findBySlug; commented type hinting

--- app/Repositories/Contracts/BlogPostRepository.php
+++ app/Repositories/Contracts/BlogPostRepository.php
@@ -2,8 +2,13 @@
 
 namespace App\Repositories\Contracts;
 
+/**
+ *  @method \Illuminate\Database\Eloquent\Collection|static[] all($columns = ['*']);
+ *  @method \Illuminate\Contracts\Pagination\LengthAwarePaginator paginate(int $perPage=10, array $columns = ['*']);
+ *  @method $this|\Illuminate\Database\Eloquent\Model create(array $properties);
+ */
+
 interface BlogPostRepository
 {
-    /*public function allLive($paginate = 10);
-    public function allLiveLatest($paginate = 10);*/
+    public function findBySlug($slug);
 }
